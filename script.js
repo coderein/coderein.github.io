fc3480780fa31a5cca58cc1fe1a4a029dfe78dcf
coderein <45535056+coderein@users.noreply.github.com>
2023-08-25 21:32:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,35 +3,33 @@
 document.addEventListener('DOMContentLoaded', function () {
   const calculateBtn = document.getElementById('calculate-btn');
   const resultElement = document.getElementById('result');
-  const additionalInfoElement = document.getElementById('additional-info');
-
-  const currentDate = new Date();
-  const twelveWeeksAgo = new Date(currentDate);
-  twelveWeeksAgo.setDate(twelveWeeksAgo.getDate() - (12 * 7));
-
-  const currentDateFormatted = formatDate(currentDate);
-  const twelveWeeksAgoFormatted = formatDate(twelveWeeksAgo);
-
-  const additionalInfo = `Today's date is ${currentDateFormatted}, all Response Submissions dated ${twelveWeeksAgoFormatted} or earlier will result in a decision term of six weeks.`;
-  additionalInfoElement.textContent = additionalInfo;
 
   calculateBtn.addEventListener('click', function () {
     const judgmentDate = new Date(document.getElementById('judgment-date').value);
     const responseDate = new Date(document.getElementById('response-date').value);
 
-    const weeksBetweenDates = Math.abs((judgmentDate - responseDate) / (1000 * 60 * 60 * 24 * 7));
-    const weeksRemaining = 12 - weeksBetweenDates;
+    const termInWeeks = calculateTermInWeeks(judgmentDate, responseDate);
 
-    let result = '';
-
-    if (weeksRemaining <= 0) {
-      result = "Term expired, Respondent should decide within 6 weeks.";
+    let resultText;
+    if (termInWeeks >= 12) {
+      resultText = "Term expired, Respondent should decide within 6 weeks.";
     } else {
-      result = `Term ongoing, Respondent should decide within ${weeksRemaining} weeks.`;
+      const remainingWeeks = Math.ceil(12 - termInWeeks); // Round up to nearest whole week
+      resultText = `Term ongoing, Respondent should decide within ${remainingWeeks} weeks.`;
     }
 
-    resultElement.textContent = result;
+    const currentDate = new Date();
+    const twelveWeeksAgo = new Date(currentDate.getTime() - (12 * 7 * 24 * 60 * 60 * 1000));
+    const additionalLine = `Today's date is ${formatDate(currentDate)}, all Response Submissions dated ${formatDate(twelveWeeksAgo)} or earlier will result in a decision term of six weeks.`;
+
+    resultElement.textContent = resultText + "\n" + additionalLine;
   });
+
+  function calculateTermInWeeks(startDate, endDate) {
+    const millisecondsPerWeek = 7 * 24 * 60 * 60 * 1000;
+    const timeDifference = Math.abs(endDate - startDate);
+    return Math.floor(timeDifference / millisecondsPerWeek);
+  }
 
   function formatDate(date) {
     const options = { year: 'numeric', month: 'long', day: 'numeric' };
